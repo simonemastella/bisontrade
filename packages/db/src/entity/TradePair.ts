62498c9c123050a345d961babc461a52d528ab0d
simonemastella <simonemastella@gmail.com>
2025-03-10 20:18:27 +0100

    upgraded pg and fine tuned tradepair model

--- packages/db/src/entity/TradePair.ts
+++ packages/db/src/entity/TradePair.ts
@@ -7,6 +7,8 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   OneToMany,
+  BeforeInsert,
+  BeforeUpdate,
 } from 'typeorm';
 import { Asset } from './Asset';
 import { Order } from './Order';
@@ -23,15 +25,40 @@ export class TradePair {
   @ManyToOne(() => Asset, { nullable: false })
   quoteAsset: Asset;
 
+  @Column({ nullable: true })
+  symbol: string;
+
   @OneToMany(() => Order, (order) => order.tradePair)
   orders: Order[];
 
-  @Column('decimal', { precision: 18, scale: 8 })
-  minimum: string;
+  @Column('decimal', { precision: 36, scale: 18 })
+  tick_size: string;
+
+  @Column('decimal', { precision: 36, scale: 18 })
+  min_order_size: string;
 
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
   updatedAt: Date;
+
+  generateSymbol() {
+    if (this.baseAsset && this.quoteAsset) {
+      this.symbol = `${this.baseAsset.name}/${this.quoteAsset.name}`;
+    }
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  validateBeforeSave() {
+    if (
+      this.baseAsset &&
+      this.quoteAsset &&
+      this.baseAsset.id === this.quoteAsset.id
+    ) {
+      throw new Error('Base asset and quote asset cannot be the same');
+    }
+    this.generateSymbol();
+  }
 }
